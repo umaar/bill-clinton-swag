419601f5af588aa2e0d08a38b082e84c9eccd800
Thomas <tmillar@stitchfix.com>
2020-02-02 23:00:34 -0800

    Remove numpy as a dependency because it is too fat

--- api/image/index.py
+++ api/image/index.py
@@ -1,5 +1,5 @@
 import io
-import numpy
+# import numpy
 import random
 import string
 import boto3
@@ -19,21 +19,36 @@ s3 = boto3.client(
 )
 
 COORDS = [
-    [(107, 238), (262, 288), (67, 384), (212, 436)], [(118, 512), (266, 546), (9, 626), (186, 692)],
+    [(107, 238), (262, 288), (67, 384), (212, 436)],
+    [(118, 512), (266, 546), (9, 626), (186, 692)],
     [(271, 517), (436, 511), (280, 651), (501, 646)],
     [(365, 300), (473, 331), (333, 425), (436, 474)]
-]
+]  # yapf: disable
+
+COEFFS = [
+    [ 6.89723654e-01, 1.88965385e-01, -4.75096770e+02, -3.17077824e-01,
+          9.82941256e-01, -8.00050766e+02, 7.65631402e-05,  -6.88638878e-05],
+    [-1.68321049e+00, -1.60938547e+00, 4.09049678e+03, 8.29622208e-01,
+         -3.61129667e+00, 7.00435390e+03, -1.70997503e-04, -1.57128511e-03],
+    [-2.22553055e+00, 1.49475933e-01, 2.10335889e+03, -1.91535176e-01,
+         -5.26721733e+00, 1.11002296e+04, -1.49193264e-04, -1.91112495e-03],
+    [ 2.54932360e+00, 6.52626842e-01, -4.50516467e+03, -8.08405475e-01,
+          2.81638036e+00, -2.19938444e+03, 6.99165082e-04, 1.65874132e-04],
+]  # yapf: disable
 
 
-def find_coeffs(source_coords, target_coords):
-    matrix = []
-    for s, t in zip(source_coords, target_coords):
-        matrix.append([t[0], t[1], 1, 0, 0, 0, -s[0] * t[0], -s[0] * t[1]])
-        matrix.append([0, 0, 0, t[0], t[1], 1, -s[1] * t[0], -s[1] * t[1]])
-    A = numpy.matrix(matrix, dtype=numpy.float)
-    B = numpy.array(source_coords).reshape(8)
-    res = numpy.dot(numpy.linalg.inv(A.T * A) * A.T, B)
-    return numpy.array(res).reshape(8)
+#
+# PRECOMPUTED IN COEFFS BECAUSE NUMPY TOO FAT
+#
+# def find_coeffs(source_coords, target_coords):
+#     matrix = []
+#     for s, t in zip(source_coords, target_coords):
+#         matrix.append([t[0], t[1], 1, 0, 0, 0, -s[0] * t[0], -s[0] * t[1]])
+#         matrix.append([0, 0, 0, t[0], t[1], 1, -s[1] * t[0], -s[1] * t[1]])
+#     A = numpy.matrix(matrix, dtype=numpy.float)
+#     B = numpy.array(source_coords).reshape(8)
+#     res = numpy.dot(numpy.linalg.inv(A.T * A) * A.T, B)
+#     return numpy.array(res).reshape(8)
 
 
 def download_image(url):
@@ -55,10 +70,12 @@ def main(album_urls: List[str]) -> io.BytesIO:
     ]
 
     for idx, album in enumerate(albums):
-        width, height = album.size
-        coeffs = find_coeffs(
-            [(0, 0), (width, 0), (0, height), (width, height)], scale(COORDS[idx], factor=4)
-        )
+        width, height = 465, 683
+        album = album.resize((width, height), resample=Image.ANTIALIAS)
+        coeffs = COEFFS[idx]
+        # coeffs = find_coeffs(
+        #     [(0, 0), (width, 0), (0, height), (width, height)], scale(COORDS[idx], factor=4)
+        # )
         album = album.transform((width * 4, height * 4), Image.PERSPECTIVE, coeffs, Image.BICUBIC)
         album = album.resize((bg.width, bg.height), resample=Image.ANTIALIAS)
         bg = Image.alpha_composite(bg, album)
