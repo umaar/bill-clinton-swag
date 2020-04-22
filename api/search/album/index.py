95597b73c51a913266e39950ff23d830f09f69b6
Thomas <tmillar@stitchfix.com>
2020-04-22 16:17:39 -0700

    Use our own proxy for images <3 last.fm

--- api/search/album/index.py
+++ api/search/album/index.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import os
 
 from bs4 import BeautifulSoup
 from flask import Flask, jsonify, request, redirect, make_response
@@ -11,12 +12,17 @@ app = Flask(__name__)
 @app.route('/<path:path>')
 def catch_all(path):
     q = request.args.get('q')
-    resp = jsonify(search(q))
+    base_url = request.headers.get('X-Now-Deployment-Url')
+    if 'localhost' in base_url:
+        base_url = f'http://{base_url}'
+    else:
+        base_url = f'https://{base_url}'
+    resp = jsonify(search(q, base_url))
     resp.headers['Cache-Control'] = 's-maxage=86400, stale-while-revalidate'
     return resp
 
 
-def search(query):
+def search(query, base_url='http://localhost:300'):
     resp = requests.get(f'https://www.last.fm/search/albums?q={query}')
     resp.raise_for_status()
 
@@ -24,10 +30,14 @@ def search(query):
 
     return [
         {
-            'url': re.sub('(\d+s)', 'avatar170s',
-                          x.find('img').attrs['src']),
-            'album': x.find('h4').a.text,
-            'artist': x.find('p').a.text,
+            'url':
+            re.sub('(\d+s)', 'avatar170s',
+                   x.find('img').attrs['src']
+                  ).replace('https://lastfm.freetls.fastly.net/i/', f'{base_url}/i/'),
+            'album':
+            x.find('h4').a.text,
+            'artist':
+            x.find('p').a.text,
         } for x in soup.findAll('div', {'class': 'album-result-inner'})
     ]
 
